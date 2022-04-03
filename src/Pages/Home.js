e31c301221073e24af81935ac7cfbc04b86208f5
PATEL96 <rajp08367@gmail.com>
2022-04-03 11:57:52 +0530

    Launch

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Pages.css'
 import image from '../Images/Eng Poster Final-01.png'
+import Notice from './Notice';
 
 function Home() {
 
     return (
         <div className='cont-home' >
+            <Notice />
             <img src={image} alt="none" className='home-img' />
         </div>
     );
---
+++ src/Pages/Notice.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Pages.css';
+
+function Notice() {
+    return (
+        <div className="cont-notice">
+            <center>
+                <div className="notice-main" >
+                    <div className="notice" >
+                        !! All Participants Are requested to pay using the QR Code given with Event !!
+                    </div>
+                </div>
+            </center>
+        </div>
+    );
+}
+
+export default Notice;
